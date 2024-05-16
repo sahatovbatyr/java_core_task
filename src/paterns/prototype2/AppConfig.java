cebce2d023ae393531202a217b4e84b09599c601
sahatovbatyr <sahatovbatyr@gmail.com>
2024-05-16 09:49:59 +0300

    prototype second

--- src/paterns/prototype2/AppConfig.java
+++ src/paterns/prototype2/AppConfig.java
@@ -1,4 +1,36 @@
 package paterns.prototype2;
 
-public class AppConfig {
+import java.util.HashMap;
+import java.util.Map;
+
+// Пример класса конфигурации приложения
+class AppConfig implements Cloneable {
+    private Map<String, String> parameters;
+
+    public AppConfig() {
+        this.parameters = new HashMap<>();
+        // Здесь мы можем загрузить параметры из файловой системы, базы данных или других источников
+        // В этом примере просто добавляем некоторые тестовые параметры
+        parameters.put("param1", "value1");
+        parameters.put("param2", "value2");
+    }
+
+    public void setParameter(String key, String value) {
+        parameters.put(key, value);
+    }
+
+    public String getParameter(String key) {
+        return parameters.get(key);
+    }
+
+    @Override
+    public AppConfig clone() {
+        try {
+            return (AppConfig) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
+
